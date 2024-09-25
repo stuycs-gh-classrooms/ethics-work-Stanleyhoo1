896b841e378f881f50a7f4804b58774de2efb7c5
stanleyhoo1 <stanleyhoo0427@gmail.com>
2024-09-25 01:20:32 -0400

    finished game

--- w05_array2d/tictactoe.py
+++ w05_array2d/tictactoe.py
@@ -5,6 +5,26 @@ game_grid = [['' for _ in range(3)] for _ in range(3)]
 game = True
 turn = True
 
+def check_win():
+    # Check rows and columns
+    for i in range(3):
+        # Check rows
+        if game_grid[i][0] == game_grid[i][1] == game_grid[i][2] != '':
+            return game_grid[i][0]
+        # Check columns
+        if game_grid[0][i] == game_grid[1][i] == game_grid[2][i] != '':
+            return game_grid[0][i]
+    # Check diagonals
+    if game_grid[0][0] == game_grid[1][1] == game_grid[2][2] != '':
+        return game_grid[0][0]
+    if game_grid[0][2] == game_grid[1][1] == game_grid[2][0] != '':
+        return game_grid[0][2]
+    # Check for draw
+    for row in game_grid:
+        if '' in row:
+            return None  # Game is not over yet
+    return 'Draw'
+
 def create_row_col():
     row = []
     for i in range(17):
@@ -41,9 +61,9 @@ def play_turn():
     global turn
     empty = True
     while empty == True:
-        print("Enter the row: ")
+        print("Enter the row (0-2): ")
         row = input()
-        print("Enter the col ")
+        print("Enter the col (0-2): ")
         col = input()
         if int(col) > 2 or int(row) > 2:
             print("Invalid move! Index out of bounds!")
@@ -62,4 +82,11 @@ def play_turn():
 while game==True:
     play_turn()
     display(grid)
+    result = check_win()
+    if result:
+        if result == 'Draw':
+            print("The game is a draw!")
+        else:
+            print(f"Player {result} wins!")
+        game = False
     
